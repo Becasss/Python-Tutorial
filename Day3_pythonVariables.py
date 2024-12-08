5f5a1a0d6bb737c13bdd650d0d6d871353efddc4
w3schools-test <raeebikash@gmail.com>
2024-12-08 19:48:06 +0545

    Update pythonVariables.py

--- Day3_pythonVariables.py
+++ Day3_pythonVariables.py
@@ -3,14 +3,24 @@
 # Variables : are containers for storing data values.
 
 # Creating Variables: Python has no command for declaring a variable.
+
 # Example 1:
 myVariable = "Hello World!"
 x = 34
-print(myVariable)
-print(x)
+print(myVariable)   # Output: "Hello World!"
+print(x)            # Output: 34
 
 
 # Example 2:
 y = 5
 y = "Hello World!"
-print(y)
+print(y)  # Output: "Hello World!"
+
+
+# Casting: If you want to specify the data type of a variable, this can be done with casting.
+
+# Example 3:
+x = str(34)
+y = int(34)
+print(x)  # Output: "34"
+print(y)  # Output: 34
