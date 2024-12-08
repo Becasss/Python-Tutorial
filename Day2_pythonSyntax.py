72b8308f0fa7b0d64d2d9633aafc5893457a496c
w3schools-test <raeebikash@gmail.com>
2024-12-08 19:37:40 +0545

    Updating Day2_pythonSyntax.py

---
+++ Day2_pythonSyntax.py
@@ -0,0 +1,33 @@
+#Python Indentation: uses indentation to indicate a block of code.
+
+
+
+# Example 1:
+if 5 > 2:
+    print("Five is greater than two!")
+
+    # Pyhton will give you error if you skip the indentation
+
+
+
+# Python Variables: In python, Variables are created when you assign a value to it:
+
+x = 5
+y  = "Hello World"
+
+print(x)
+print(y)
+
+
+# Comments : It has commenting capability for the purpose of in-code documentation.
+
+
+# This is a comment line.
+print("This is a comment line")
+
+# Multi-line comments
+
+"""
+This is a multi-line
+comment.
+"""
