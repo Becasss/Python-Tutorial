77f31aba5e433ad5b61f6a937978673aa0d2de10
w3schools-test <raeebikash@gmail.com>
2024-12-08 19:46:26 +0545

    Update Day3_pytonVariables.py

---
+++ Day3_pythonVariables.py
@@ -0,0 +1,16 @@
+
+
+# Variables : are containers for storing data values.
+
+# Creating Variables: Python has no command for declaring a variable.
+# Example 1:
+myVariable = "Hello World!"
+x = 34
+print(myVariable)
+print(x)
+
+
+# Example 2:
+y = 5
+y = "Hello World!"
+print(y)
