eac5a6f716a4517941c72a3d7cb6092aae75df6b
w3schools-test <raeebikash@gmail.com>
2024-12-10 14:16:40 +0545

    Update day6_pythonBooleans.py

---
+++ Day6_pythonBooleans.py
@@ -0,0 +1,33 @@
+
+# Python Booleans : represents one of two values: True or False
+
+# Example 1
+print(10 > 9)           # Output : True
+print (10 == 9)         # Output : False
+print(10 < 9)           # Output : False
+
+# Example 2
+a = 200
+b = 22
+
+if b > a:
+    print("B is greater than a")
+else:
+    print("A is greater than or equal to B")
+
+
+
+
+
+# Evaluate values and variables
+
+# Example:
+print(bool("Hello"))   # Output : True
+print(bool(12))         # Output :True
+
+
+
+# Most Values are True
+print(bool("abc"))      # Output : True
+print(bool(123))        # Output : True
+print(bool(["apple", "orange"]))  # Output : True
