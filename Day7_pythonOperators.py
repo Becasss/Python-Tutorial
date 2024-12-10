c9e0303024ec3e703004005bc9a629cf06ff3a79
w3schools-test <raeebikash@gmail.com>
2024-12-10 17:02:08 +0545

    Create Day7_pythonOperators.py

---
+++ Day7_pythonOperators.py
@@ -0,0 +1,64 @@
+
+# Python Operators
+
+# Python Arithmetic Operators
+x = 5
+y = 10
+print(x + y)    # Output : 15 - Addition
+
+print(x - y) # Output : -5 - Subtraction
+
+print(x * y) # Output : 50 - Multiplication
+
+print(x / y) # Output : 0.5 - Division
+
+print(x % y) # Output : 5 - Modulus
+
+print(x ** y) # Output :9765625 - Exponentiation
+
+print(x // y) # Output : 0 - Floor Division
+
+
+
+
+# Python Assignment Operators
+
+q = 10
+print(q) # Output : 10 - Assignment
+
+q += 5
+print(q) # Output : 15 - Addition Assignment
+
+q -= 5
+print(q) # Output : 10 - Subtraction Assignment
+
+q *= 5
+print(q) # Output : 50 - Multiplication Assignment
+
+q /= 5
+print(q) # Output : 10.0 - Division Assignment
+
+
+
+# Python Comparison Operators : Comparison Operators are used to compare two values
+
+a = 3
+b = 7
+
+print(a == b) # Output :False - Equal Comparison
+
+print(a != b) # Output :True - Not Equal Comparison
+
+print(a > b) # Output :False - Greater Comparison
+
+print(a < b) # Output : True - Less Comparison
+
+print(a >= b) # Output : False - Greater or Equal Comparison
+
+print(a <= b) # Output : True - Less or Equal Comparison
+
+
+
+# Python Logical Operators
+
+print(a < 5 and b < 10)  # Output : 
