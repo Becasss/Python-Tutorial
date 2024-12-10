1ecc72ef97d3ea7719b3b347bfcaad87ed5ed606
w3schools-test <raeebikash@gmail.com>
2024-12-10 14:32:34 +0545

    Update Day6pythonBooleans.py

--- Day6_pythonBooleans.py
+++ Day6_pythonBooleans.py
@@ -31,3 +31,45 @@ print(bool(12))         # Output :True
 print(bool("abc"))      # Output : True
 print(bool(123))        # Output : True
 print(bool(["apple", "orange"]))  # Output : True
+
+
+
+# Some Values are False
+print(bool("False")) # Output : False
+print(bool(None)) # Output : False
+print(bool(0))  # Output : False
+print(bool('')) # Output : False
+print([]) # Output : False
+print({}) # Output : False
+
+
+
+class myclass():
+    def __len__(self):
+        return 0
+myobj = myclass()
+print(bool(myobj))
+
+
+
+# Function can return a Boolean
+# Example:
+def myFunction():
+    return True
+print(myFunction()) # Output : True
+
+
+# Example:
+def myfunctions():
+    return True
+if myfunctions():
+    print("Yes")
+else:
+    print("No")
+
+
+
+
+# isinstance function
+x = 200
+print(isinstance(x, int)) # Output : True
