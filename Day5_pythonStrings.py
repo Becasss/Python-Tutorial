ef3920e5356826ba302822fdf11e6e65083e60c8
w3schools-test <raeebikash@gmail.com>
2024-12-10 13:19:44 +0545

    Update Day5_pythonStrings.py

---
+++ Day5_pythonStrings.py
@@ -0,0 +1,33 @@
+
+
+# Strings : surrounded by either single quotes or double quotation marks. - "Hello" or 'Hello'.
+
+
+# Assign String to a variable:
+ #Example:
+a = "Hello, world!"
+print(a) # Output: Hello
+
+
+# Multiline Strings:
+b = """Lorem ipsum dolor sit amet, consectetur adip
+elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+ut aliquip ex ea commodo consequat."""
+print(b)  
+#Note: in the result, the line breaks are inserted at the same position as in the code. 
+
+
+
+# Strings are arrays:
+print(a[1])     # Output : world
+
+
+
+# Looping through a string
+# Example:
+for x in "Banana":
+    print(x)
+
+# String Length
+print(len(b)) # Output :
