d8001b17c4b87c5db5d3b17828436bb6deef82f0
w3schools-test <raeebikash@gmail.com>
2024-12-11 11:32:58 +0545

    Create day8pythonLists.py

---
+++ Day8_pythonLists.py
@@ -0,0 +1,27 @@
+
+# Python Lists : mylist = ["apple", "orange", "cherry"]
+
+# List are used to store multiple items in a single variable
+# Example:
+thislist = ['apple', 'orange', 'cherry']
+print(thislist)
+
+
+
+# Lits Items : are orderd, changeable and allow duplicate values
+
+
+# Ordered : if you add new items to a list, the new items will be placed at the end of the list
+
+# Changeable: meaning that we can change, add, and remove items in a list after it has been created.
+
+# Allow Duplicates : Since list are indexed, lists can have items with the same value.
+# Example: 
+list1 = ['apple', 'orange', 'cherry', 'apple']
+print(list1)
+print(list1[3])
+
+
+# List Length: len() function
+
+print(len(list1))
