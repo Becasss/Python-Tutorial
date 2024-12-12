a9024eef2e9cbbe1b8863ece136610f8c6173da9
w3schools-test <raeebikash@gmail.com>
2024-12-12 17:32:11 +0545

    Update Day9pythonTuples.py

--- Day9_pythonTuples.py
+++ Day9_pythonTuples.py
@@ -34,3 +34,83 @@ y = list(x)
 y[1] = 'kiwi'
 x = tuple(y)
 print(x)  # Output: ('apple', 'kiwi', 'orange')
+
+
+# Add the items:
+thistuple = ('banna', 'cherry', 'apple')
+y = list(thistuple)
+y.append('orange')
+thistuple = tuple(y)
+print(thistuple)  # Output: ('banna', 'cherry', 'apple', 'orange')
+
+
+# Remove Items:
+
+thistuple = ('apple', 'banana', 'cherry')
+y = list(thistuple)
+y.remove('apple')
+thistuple = tuple(y)
+print(thistuple)  # Output: ('banana', 'cherry')
+
+
+
+
+# Unpacking a Tuple:
+fruits = ('apple', 'banana', 'cherry')
+(green, blue, yellow) = fruits
+print(green) # Output: apple
+print(blue)  # Output: banana
+
+
+
+# Using Asterisk*:
+
+fruits = ('apple', 'banana', 'cherry', 'raspberry')
+(green, blue, *yellow) = fruits
+print(green) # Output: apple
+print(blue)  # Output: banana
+print(yellow)  # Output: ['cherry', 'raspberry']
+
+
+
+# Loop through a Tuple:
+
+thistuple = ('apple', 'banana', 'cherry', 'raspberry')
+for x in thistuple:
+    print(x)
+
+
+
+# Loop through the Index Numbers
+
+thistuple = ('apple','banana','cherry','raspberry')
+for i in range(len(thistuple)):
+    print(thistuple[i])
+
+
+
+
+# Using a while loop
+
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+    print(thistuple[i])
+    i = i + 1
+
+
+
+# python - Join Tuples
+
+# Join Two Tuples
+tuple1 = ("a", "b", "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)  # Output: ('a', 'b', 'c', 1, 2, 3)
+
+
+# Multiply Tuples:
+fruits = ('apple', 'banana', 'cherry')
+mytuple = fruits * 2
+print(mytuple)  # Output: ('apple', 'banana', 'cherry', 'apple', 'banana', 'cherry')
