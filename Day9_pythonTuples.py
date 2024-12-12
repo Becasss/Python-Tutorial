3908175b2ee8d490237b9fd29f815cc687f1e5b1
w3schools-test <raeebikash@gmail.com>
2024-12-12 17:19:27 +0545

    Update day9pyth0nTuples.py

--- Day9_pythonTuples.py
+++ Day9_pythonTuples.py
@@ -14,3 +14,23 @@ print(thistuple)
 # Negative Indexing:
 print(thistuple[-2])    #Output : banana
 
+
+# Check If items:
+
+if "apple" in thistuple:
+    print("Yes, apple is in thistuple") # Output : Yes, apple is in thistuple
+
+
+
+
+
+# Updte Tuple : Tuples are unchangeable, meanig that you can't change3 , add or remove items once the tuple is created.
+
+# Change the tuple: 
+
+# Example:
+x = ('apple', 'banana', 'orange')
+y = list(x)
+y[1] = 'kiwi'
+x = tuple(y)
+print(x)  # Output: ('apple', 'kiwi', 'orange')
