d97d8acf60561aed4194bd25396b8a360e47a4b0
w3schools-test <raeebikash@gmail.com>
2024-12-13 23:01:50 +0545

    Update day10pythonsets.py

--- Day10_PythonSets.py
+++ Day10_PythonSets.py
@@ -68,3 +68,107 @@ tropical = {"pineapple", "mango", "papaya"}
 thisset.update(tropical)
 
 print(thisset)
+
+
+
+# Add Any Iterable:
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "Oange"]
+thisset.update(mylist)
+print(thisset)
+
+
+
+
+
+# Remove Set Items: To remove an item in a set, use the remove() or the discard() method
+thisset = {"apple", "banana", "cherry"}
+thisset.remove("banana")
+print(thisset)
+
+
+thisset.discard("apple")
+print(thisset)
+
+
+
+
+# Pop() Method: remove a random item by using the pop() method 
+thisset = {"apple", "banana", "Chrrry"}
+x = thisset.pop()
+print(x) # removed item
+print(thisset) # the set after removal
+
+
+
+# Clear() Method:empties the set
+
+thisset = {"apple", "banana", "cherry"}
+thisset.clear()
+print(thisset)
+
+
+
+# del keyword will delete the set completely
+#thisset = {"apple", "banana", "cherry"}
+#del thisset
+#print(thisset)
+
+
+
+
+# Python - Loop Sets
+# Loop Items - Loop through the set, and print the values
+thisset = {"apple","banana","cherry"}
+for x in thisset:
+    print(x)
+
+
+
+# Python - Join Sets
+# The union() and udate() methods joins all items form both sets.
+# The intersection() method keeps ONLY the duplicates
+# The difference() method kees the items from the first set that are not in the other sets.
+# The symmetric_difference() method keeps only all items EXCEPT the duplicates.
+
+
+# Union () Methods:
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3, 4}
+set3 = set1.union(set2)
+print(set3)
+
+
+
+# Union  () methods:
+set1 = {"a", "b", "c"}
+set2 = {1,2,3}
+set3 = set1 | set2
+print(set3)
+
+
+
+# join multiple sets with the union() method
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = {"d", "e", "f"}
+set4 = set1.union(set2, set3)
+print(set4)
+set5 = set1 | set2 | set3 | set4
+print(set5)
+
+
+
+# Join a set and a tuple - union() method allows you to join a  set with others data types, like lists or tupls
+x = {"a", "b", "c", "d"}
+y = (1,2 ,3)
+z = x.union(y)
+print(z)
+
+
+
+# Update() method inserts all the items from one set into another
+x = {"a", "b", "c"}
+y = {"d", "e", "f"}
+x.update(y)
+print(x)
