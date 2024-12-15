ffd21da2a3381174acfb10355dbd2c9cd4adc830
w3schools-test <raeebikash@gmail.com>
2024-12-15 09:57:07 +0545

    Update Day11pythonDictionaries.py

--- Day11_PythonDictionaries.py
+++ Day11_PythonDictionaries.py
@@ -13,3 +13,28 @@ print(thisdict) # Output: {'brand': 'Ford', 'model': 'Mustang', 'year': 1964}
 
 
 
+# Dictionary Items are ordered, changeable and do not allow duplicates.
+
+print(thisdict['model'])   # output: Mustang
+
+
+
+
+# Duplicates are Not Allowed : Dictionaries cannot have two items with the same key:
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "model": "Fiesta"
+}
+print(thisdict) # Output: {'brand': 'Ford', 'model':'Fiesta', 'year':1964}
+
+
+# Dictionary Length : len() function
+
+print(len(thisdict)) # Output: 3
+
+# type() function
+
+print(type(thisdict)) # Output: dict
