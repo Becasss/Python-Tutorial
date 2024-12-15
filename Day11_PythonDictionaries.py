ac45f93cb4f40f402120393cba2ea8d3615bfb58
w3schools-test <raeebikash@gmail.com>
2024-12-15 09:44:36 +0545

    Create Day11pythonDictionaries.py

---
+++ Day11_PythonDictionaries.py
@@ -0,0 +1,15 @@
+
+# Python Dictionaries : Dictionaries are used to store data values in key:values pairs.
+
+
+# Example: Create and print a dictionary
+thisdict ={
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+print(thisdict) # Output: {'brand': 'Ford', 'model': 'Mustang', 'year': 1964}
+
+
+
