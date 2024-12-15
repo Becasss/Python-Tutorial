fefdfa87b1d188e4bcfc6cf06046b6504528bca1
w3schools-test <raeebikash@gmail.com>
2024-12-15 13:08:05 +0545

    Update Day11pythondictionaries.py

--- Day11_PythonDictionaries.py
+++ Day11_PythonDictionaries.py
@@ -69,3 +69,112 @@ print(y) # Output: "John"
 
 
 
+# Get Keys() - will return a list of all the keys in the dictionary.
+
+a = thisdict.keys()
+print(a)  # Output: ['name], 'age', 'city']
+
+
+# Example: 
+car = {
+    "name" : "Ford",
+    "model": "Mustang",
+    "year" : 1964
+}
+
+b = car.keys() # before the change
+
+print(b) # Output: ['name', 'model', 'year']
+car['color'] = "white" 
+
+print(b) # Output: ['name', 'model, 'year', 'color']
+
+
+# Get Values() - will return a list of values in the dictionary.
+
+c = thisdict.values()
+print(c) # Output: ['New York', 34, 'United States']
+
+
+# Example:
+
+fruits = {
+    "apple": 1,
+    "banana": 2,
+    "cherry": 3
+}
+d = fruits.values()
+print(d) # before the change
+fruits["grape"] = 4
+print(d) # after the change
+
+
+
+# Get Items() - will return a list of tuples containing each key-value pair in the dictionary.
+
+
+e = fruits.items()
+print(e) # Output: [('apple', 1), ('banana', 2), ('cherry', 3), ('grape', 4)]
+
+fruits["kiwi"] = 5
+print(e) # after the change
+
+
+
+
+# Check if Key Exists() - 
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+if "model" in thisdict:
+    print("Yes, 'mode' is of the keys in thisdict")
+
+
+
+
+# Python - Chnage Dictionary Items
+
+# Change Values:
+thisdict = {"brand": "Ford",
+            "model": "Mustang",
+            "year": 1964
+            }
+thisdict["year"] = 2020
+print(thisdict) # Output: {'brand': 'Ford', 'model': 'Mustang', 'year': 2020}
+
+
+# Update Dictionary - update() method will update the dictionary with the from the given arguements
+
+thisdict = {"brand": "Ford",
+            "model": "Mustang",
+            "year": 1964
+            }
+thisdict.update({"year": 2020, "color": "red"})
+print(thisdict) # Output: {'brand': 'Ford', 'model': 'Mustang', 'year': 2020, 'color': 'red'}
+
+
+
+# Add items: done by using a new index key and assign a value to it.
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year" :2020
+}
+thisdict["color"] = "Red"
+print(thisdict) # Output: {'brand': 'Ford', 'model': 'Mustang', 'year': 2020, 'color': 'Red'}
+
+
+# Update Dictionary - will update the dictionary with items from a given argument.
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year" :2020
+}
+
+thisdict.update({"color": "Red"})
+print(thisdict) # Output: {'brand': 'Ford', 'model': 'Mustang', 'year': 2020, 'color': 'Red'}
