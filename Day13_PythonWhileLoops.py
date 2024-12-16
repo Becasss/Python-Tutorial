f15964439f2561e9c731e6aa35999797c42bf344
w3schools-test <raeebikash@gmail.com>
2024-12-16 15:29:57 +0545

    Update Day13pythonwhileloops.py

--- Day13_PythonWhileLoops.py
+++ Day13_PythonWhileLoops.py
@@ -1,1 +1,12 @@
-# Python While Loops:
+# Python While Loops: Python has two primitive loop commands - while loops and for loops
+
+
+# While Loop: a set of statements as long as a condition is true.
+
+# Example: 
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+
