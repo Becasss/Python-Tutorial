951cad2ef8faf094f9a6c6bc982ccb80e9929d95
w3schools-test <raeebikash@gmail.com>
2024-12-16 16:53:32 +0545

    looping through a string

--- Day14_PythonForLoops.py
+++ Day14_PythonForLoops.py
@@ -5,3 +5,24 @@
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
     print(x)
+
+
+# lOOPING THROUGH A STRING
+
+for x in "banana":
+    print(x)
+
+
+# LOOPING THROUGH A DICTIONARY
+
+for x in "Banana":
+    print(x)  
+
+
+# The Break Statement:   we can stop the loop before it has looped through all the items.
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
