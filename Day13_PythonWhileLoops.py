167c315379f449dabe7cabffe0ad95ccf3ac7f6b
w3schools-test <raeebikash@gmail.com>
2024-12-16 16:06:16 +0545

    Continue statement

--- Day13_PythonWhileLoops.py
+++ Day13_PythonWhileLoops.py
@@ -22,3 +22,10 @@ while i < 6:
 
 
 # Continue Statement: can stop the current iteration and continue with the next.
+
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
