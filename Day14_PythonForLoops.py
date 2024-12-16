295b838a7fa4f7f9b600a1ebf1536c0f3e68ffd9
w3schools-test <raeebikash@gmail.com>
2024-12-16 16:14:06 +0545

    Creating file pythonForLoops.py

---
+++ Day14_PythonForLoops.py
@@ -0,0 +1,7 @@
+# Python For Loops - for loop is used for iterating over a sequence(either a set, a tuple, a dictionary, a set or a string).
+
+
+# Example :
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
