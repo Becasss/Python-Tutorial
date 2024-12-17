7288f527182feb8aaa414a49a5dd14413dcf393e
w3schools-test <raeebikash@gmail.com>
2024-12-17 11:16:45 +0545

    Learning Nested Loop

--- Day14_PythonForLoops.py
+++ Day14_PythonForLoops.py
@@ -58,4 +58,14 @@ for x in range(6):
     print(x)
 else:
     print("Finally finished!")
-    
+
+
+
+# Nested Loops : A nested loop is a loop inside a loop.
+
+adj = ['red', 'big', 'tasty']
+fruits = ['apple', 'banana', 'cherry']
+for x in adj:
+    for y in fruits:
+        print(x, y)
+        
