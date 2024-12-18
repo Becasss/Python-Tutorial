8930309266448d3b4472d7884118f9aa706f7bdf
w3schools-test <raeebikash@gmail.com>
2024-12-18 13:04:16 +0545

    Learning Array

--- DAY17_PythonArrays.py
+++ DAY17_PythonArrays.py
@@ -1,3 +1,25 @@
 
 # Python Arrays - python doesn't support arrays but Python_Lists can be used instead.
 
+
+# Example: Create an array containing car names:
+
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+
+
+
+# Access the Elements of an Array - by refering to the index number.
+
+x = cars[0]
+print(x)        # Output : Ford
+
+
+cars[0] = "TOyota"
+print(cars)        # Output : ['TOyota', 'Volvo', 'BMW']
+
+
+
+
+# The Length of an Array
+print(len(cars)) # Output : 3
