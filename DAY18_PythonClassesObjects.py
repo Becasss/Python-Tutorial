c845d7fcbb26eb56a8da48d6efa6a9393df7399e
w3schools-test <raeebikash@gmail.com>
2024-12-18 13:52:16 +0545

    Object Methods

--- DAY18_PythonClassesObjects.py
+++ DAY18_PythonClassesObjects.py
@@ -71,3 +71,50 @@ print(p1)
 
 
 # Objects Methods
+
+# Example: Insert a function that prints a greeting, and execute it on the p1 object:
+class Person:
+    def __init__(self,name, age):
+        self.name = name
+        self.age = age
+    
+    def greeting(self):
+        print(f"Hello my name is {self.name}.")
+
+p1 = Person("Bikash", 34)
+p1.greeting()  # Output : Hello my name is Bikash.
+
+
+
+# The Self Parameter - The self parameter is a reference to the current instance of the class, 
+# and is used to access variables that belong to the class.
+
+# Example: Use the words mysillyobject and abc instead of self:
+
+
+class Person:
+    def __init__(mysillyobject, name,age):
+        mysillyobject.name = name
+        mysillyobject.age = age
+
+    def greeting(abc):
+        print(f"Hello my name is {abc.name}." )
+
+p1 = Person("John", 23)
+p1.greeting()  # Output : Hello my name is John.
+
+
+
+
+# Modify Object Properties :
+class Person:
+    def __init__(self,name, age):
+        self.name = name
+        self.age = age
+    def myfunc(self):
+        print(f"Hello my name is {self.name}")
+
+p1 = Person("John", 23)
+
+p1.age = 22
+print(p1.age)
