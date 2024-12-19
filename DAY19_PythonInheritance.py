34a049a654a20c070d6f9f8fe6269420f4514155
w3schools-test <raeebikash@gmail.com>
2024-12-19 13:57:30 +0545

    Learnig Add methods

--- DAY19_PythonInheritance.py
+++ DAY19_PythonInheritance.py
@@ -125,10 +125,31 @@ class Person:
 
 class Student(Person):
     def __init__(self,fname,lname,year):
-        super().__init__(fname,lname, year)
+        super().__init__(fname,lname)
         self.graduationyear = year
 
 x = Student("Mike", "Olsen", 2089)
 print(x.graduationyear)
 
 
+
+
+# Add Methods:
+# Example: Add a method called welcome to the student class:
+
+class Person:
+    def __init__(self,fname,lname):
+        self.firstname = fname
+        self.lastname = lname
+    def printname(self):
+        print(self.firstname, self.lastname)
+
+class Student(Person):
+    def __init__(self,fname,lname,year):
+        super().__init__(fname,lname)
+        self.graduationyear = year
+
+    def welcome(self):
+        print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+x = Student("Batista", "John", 34)
+x.welcome()
