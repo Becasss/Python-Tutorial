f290e51592dd2c2eb900fe51b0b5be978c8d7ff8
w3schools-test <raeebikash@gmail.com>
2024-12-19 12:22:18 +0545

    Learning add __init__() function

--- DAY19_PythonInheritance.py
+++ DAY19_PythonInheritance.py
@@ -44,3 +44,47 @@ class Student(Person):
 
 x = Student("Mike", "Olsen")
 x.printname()
+
+
+
+
+
+# ADD the __init__ () Function: 
+
+# Example: Add the __init__() function to the student class:
+
+class Person:
+    def __init__(self,fname,lname):
+        self.firstname = fname
+        self.lastname = lname
+    def printname(self):
+        print(self.firstname, self.lastname)
+
+class Student(Person):
+    def __init__(self,fname,lname):
+        Person.__init__(self,fname,lname)
+
+x = Student("Mike", "Olsen")
+
+x.printname()
+
+
+
+
+
+# Use the super() Function - will make the child inherit all the methods and properties from its parent.
+
+class Person:
+    def __init__(self,fname,lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+        print(self.firstname, self.lastname)
+
+class Student(Person):
+    def __init__(self,fname,lname):
+        super().__init__(fname,lname)
+
+z = Student("John", "John")
+z.printname()
