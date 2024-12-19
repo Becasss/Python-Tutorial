90bbc497c00f5e826e620d37d9d0b6d31f1dc64b
w3schools-test <raeebikash@gmail.com>
2024-12-19 12:01:27 +0545

    Learning Python Inheritance

--- DAY19_PythonInheritance.py
+++ DAY19_PythonInheritance.py
@@ -1,3 +1,24 @@
 
 
-# Pyhton Inheritance - 
+# Pyhton Inheritance - allows us to define a class that inherits all the methods and properties from another class.
+
+# Parent Class - is the class being inherited from, also called base class.
+# Child Class - is the class that inherits from another class, also called derived class.
+
+
+# Create a parent Class.
+
+# Example: Create a class named Person, with firstname and lastname properties and a printname method:
+
+class Person: 
+    def __init__(self,firstname,lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+    def printname(self):
+        print(self.firstname, self.lastname)
+
+    # Use the Person class to create an object and the execute thr printname method:
+
+x = Person("Bikash", "Yamphu Rai")
+x.printname()
