355c3d83cc4f8ba26f688df228d8376a793116fe
w3schools-test <raeebikash@gmail.com>
2024-12-20 10:35:23 +0545

    Learning function polymorphism - len() function

--- DAY21_PythonPolymorphism.py
+++ DAY21_PythonPolymorphism.py
@@ -1,2 +1,16 @@
 
 # Python Polymorphism - means many forms and in programming it refers to methods/functions/operators with the same name that can be executed on many objects or classes.
+
+
+# Function Polymorphism - len() function
+
+# String - for strings len() returns the number of characters.
+x = "Hello World!"
+print(len(x))  # Output: 12
+
+
+
+# Tupe - for tuples len() returns the number of items in the tuple.
+
+mytuple = ("apple", "orange", "cherry")
+print(len(mytuple))  # Output: 3
