7a3afd724df66cf34c2c043b05b4273c2a212080
w3schools-test <raeebikash@gmail.com>
2024-12-20 12:16:36 +0545

    Update day21PythonPolymorphism.py

--- DAY21_PythonPolymorphism.py
+++ DAY21_PythonPolymorphism.py
@@ -61,3 +61,41 @@ plane1 = Plane("Boeing", "747")  # Create a Plane object
 
 for x in (car1, boat1, plane1):
     x.move()  # Output: Drive!, Sail!, Fly!
+
+
+
+# Inheritance Class Polymorphism - 
+
+# Example: Create a class called vechicle and make car, Boat, Plane child classes of vechicle.
+
+class Vehicle:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def move(self):
+        pass
+
+    def move(self):
+        print("Move")
+
+class Car(Vehicle):
+    pass
+
+class Boat(Vehicle):
+    def move(self):
+        print("Sail")
+
+class Plane(Vehicle):
+    def move(self):
+        print("Fly")
+
+
+car1 = Car("Ford", "Mustang")  # Create a Car Object
+boat1 = Boat("Ibiza", "Touring 20") # Create aboat Object
+plane1 = Plane("Boeing", "747")  # Create a Plane object
+
+for x in (car1, boat1, plane1):
+    print(x.brand)
+    print(x.model)
+    x.move()  # Output: Ford, Mustang, Move, Ibiza, Touring 20, Sail, Boeing, 747, Fly  
