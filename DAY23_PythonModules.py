8fa76abb61ad04a39fe5361abc723e662ca20904
w3schools-test <raeebikash@gmail.com>
2024-12-20 14:39:36 +0545

    Update for module

---
+++ DAY23_PythonModules.py
@@ -0,0 +1,22 @@
+
+
+# Python Modules - cosider a module to be the same as code library.
+
+# Create a module 
+
+# Example : Save this code in a file named mymodule.py
+"""
+def greeting(name):
+print("Hello, " + name)"""
+
+# Use a module - by using the import statement
+
+import mymodule
+mymodule.greeting("Jonathan")
+
+# Note: When using a function from a module, use the syntax: module_name.function _name.
+
+
+
+
+# 
