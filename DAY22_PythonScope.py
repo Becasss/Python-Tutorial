1507f99c4e592189f795af22537bb5acdf2b67d6
w3schools-test <raeebikash@gmail.com>
2024-12-20 12:25:57 +0545

    Learning Global Scope.

--- DAY22_PythonScope.py
+++ DAY22_PythonScope.py
@@ -21,3 +21,18 @@ def myfunc():
         print(x)
     myinnerfunc()
 myfunc()
+
+
+
+
+# Global Scope:
+
+# Example: A variable created outside of a function is global and can be used by anyone.
+
+x = 300
+
+def myfunc():
+    print(x)
+myfunc()
+
+print(x)
