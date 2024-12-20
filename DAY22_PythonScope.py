b56364964f1fe5a70022becf72595cc10e23bd0a
w3schools-test <raeebikash@gmail.com>
2024-12-20 12:21:58 +0545

    Learning local scope

--- DAY22_PythonScope.py
+++ DAY22_PythonScope.py
@@ -1,2 +1,10 @@
 
 # Python Scope: A variable is only available from inside the region it is created. This is called Scope.
+
+# Local Scope: A variable created inside a function belongs to the local scope of that function, and can only be used inside that function.
+
+def myFunct():
+    x = 300
+    print(x)
+
+myFunct()
