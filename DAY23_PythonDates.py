93e1bb3b7deb4cfb3251860b7879668ccae767ee
w3schools-test <raeebikash@gmail.com>
2024-12-21 18:14:28 +0545

    Learning Python Dates

--- DAY23_PythonDates.py
+++ DAY23_PythonDates.py
@@ -1,2 +1,12 @@
 
-# Python Dates - we can import a modue named datetime to work with dates as date objects.
+# Python Dates - we can import a module named datetime to work with dates as date objects.
+
+
+# Example: Import the datetime module and display the current date.
+import datetime
+
+x = datetime.datetime.now()
+
+print(x)
+
+
