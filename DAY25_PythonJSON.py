1c52207451efadb3705942b9a491a961da4c4372
w3schools-test <raeebikash@gmail.com>
2024-12-21 20:33:23 +0545

    learn format the result

--- DAY25_PythonJSON.py
+++ DAY25_PythonJSON.py
@@ -70,3 +70,49 @@ print(y)
 
 
 
+# Format the Result  
+
+# Example: Use the indent parameter to define the number of index.
+import json
+
+x = {
+    "name": "Dhan Bahadur Rai",
+    "age":30,
+    "married": True,
+    "divorced":False,
+    "children": ("Udash", "Bikash", "Bishal"),
+    "pets": None,
+    "cars":[
+        {"model": "BMW 230", "mpg": 25.6},
+        {"model": "Ford Edges", "mpg":24.1}
+    ]}
+
+
+
+# use four indents to make it easier to read thee result.
+
+print(json.dumps(x, indent=4, separators=(".", "=")))
+
+
+
+# Order the Result - json.dumps() method has parameters to order the keys in the result.
+
+
+# Example: Use the sor_keys() parameter to specify if the rsult should be shorted or not.
+
+import json
+
+x = {
+    "name": "Dhan Bahadur Rai",
+    "age":30,
+    "married": True,
+    "divorced":False,
+    "children": ("Udash", "Bikash", "Bishal"),
+    "pets": None,
+    "cars":[
+        {"model": "BMW 230", "mpg": 25.6},
+        {"model": "Ford Edges", "mpg":24.1}
+    ]}
+
+# sort the result alphabetically by keys:
+print(json.dumps(x, indent=4, sort_keys=True))
