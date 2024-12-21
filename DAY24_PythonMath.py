90f0b87c621c90a9bb70f05dd70cfd3acc8e27f7
w3schools-test <raeebikash@gmail.com>
2024-12-21 18:39:39 +0545

    Create Day24PythonMath.py

---
+++ DAY24_PythonMath.py
@@ -0,0 +1,13 @@
+
+# Python  Math :
+
+# Built-in Math Functions - min() and max() functions can be used to fing the lowest or hightest value in an iterable.
+
+x = min(8,7,1,3,5,6,0,2,6)
+
+y = max(8,7,1,3,5,6,0)
+
+print(x)
+print(y)
+
+# The abs() function returns the absolute (positive) value of the specified number.
