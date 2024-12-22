91eb9c90accf6574b5764c836ed3ac0d01ec654e
w3schools-test <raeebikash@gmail.com>
2024-12-22 11:55:34 +0545

    Learning raise an exception

--- DAY29_PythonTryExcept.py
+++ DAY29_PythonTryExcept.py
@@ -69,3 +69,22 @@ try:
 
 except: 
     print("Something went wrong when opening the file.")
+
+
+
+
+# Raise an exception - as a python developer you can choose to throw an exception if a condition occurs.
+
+# Example: Raise an error and stop the program if x is lower than 10.
+
+x = -1
+
+if x < 0:
+    raise Exception("SOrry, No numbers below zero")
+
+
+# Example: Raise a TypeError if x is not an integer.
+x = "hello"
+
+if not type(x) is int:
+    raise TypeError("Only integer are allowed")
