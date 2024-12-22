ba91ca760d325cef38b5464bb786d7b81c12f013
w3schools-test <raeebikash@gmail.com>
2024-12-22 10:26:05 +0545

    Learning findall() function

--- DAY27_PythonRegEx.py
+++ DAY27_PythonRegEx.py
@@ -27,3 +27,11 @@ else:
 
 
 # The findall() function - returns a list containing all matches.
+
+# Example: Print a list of all matches.
+import re
+
+txt = "THe rain in Spain"
+x = re.findall("ai", txt)
+print(x)        # Output: ['ai', 'ai']
+
