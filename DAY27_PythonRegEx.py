340a8d28bbf767ca0692c34461a4bae9bc467028
w3schools-test <raeebikash@gmail.com>
2024-12-22 10:24:19 +0545

    Learning RegEx in Python

--- DAY27_PythonRegEx.py
+++ DAY27_PythonRegEx.py
@@ -3,3 +3,27 @@
 
 
 # RegEx Module - built-in package called re, which can be used to work with Regular Expressons.
+
+# Example : Import re module - import re
+
+
+# RegEx in Python 
+# Example: Search the string to see if it starts with "THE" and ends with "Spain":
+
+import re
+
+# check if the string starts with "The" and ends with "Spain"
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+
+if x: 
+    print("YES! The string starts with 'The' and ends with 'Spain'")
+else:
+    print("No match")
+
+
+
+
+
+# The findall() function - returns a list containing all matches.
