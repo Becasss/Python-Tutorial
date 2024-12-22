5a5577dbd517f8c2a8d0301fe3768eed77ef3620
w3schools-test <raeebikash@gmail.com>
2024-12-22 12:12:26 +0545

    Learning perform operators in F- strings.

--- DAY31_PythonStringFormatting.py
+++ DAY31_PythonStringFormatting.py
@@ -24,3 +24,28 @@ print(txt)
 price = 59
 txt = f"The prics is {price:.2f} dollars."
 print(txt)  # Output : The price is 59.00 dollars.
+
+
+
+# Perform Operations in F - strings.
+
+# Example: Perform a math operation in the placeholders and return the result.
+
+txt = f"The price is {20 * 39} dollars."
+print(txt)  # Output : The price is 780 dollars.
+
+
+# Example: Add taxes before displaying the price.
+
+price = 59
+tax = 0.25
+txt = f"the price is {price * tax} dollars."
+print(txt)  # Output : the price is 14.75 dollars.
+
+
+# You can if...else statements insdie the placeholders.
+# Return "Expensive" if the price is over 50, otherwise return "Cheap:
+price = 49
+txt  = f"It is very {"Expensive" if price > 50 else "cheap"}."
+
+print(txt)
