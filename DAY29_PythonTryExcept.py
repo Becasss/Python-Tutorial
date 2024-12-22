55be75df3e8c0baca4297f2206ebaa312d2e22b8
w3schools-test <raeebikash@gmail.com>
2024-12-22 11:22:13 +0545

    Created Day29PythonTryExcept.py

---
+++ DAY29_PythonTryExcept.py
@@ -0,0 +1,9 @@
+
+# try block lets you test a block of code for errors.
+
+# except block lets you handle the error
+
+# else block lets you execute code when there is on error
+
+# finally block lets you execute code, regardless of the result of the  try- and except blocks.
+
