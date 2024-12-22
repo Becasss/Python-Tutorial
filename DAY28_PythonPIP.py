3880361a6788c3cb54ffc9f39b4f77f5cf26487a
w3schools-test <raeebikash@gmail.com>
2024-12-22 11:18:30 +0545

    Creating day28pythonPIp.py

---
+++ DAY28_PythonPIP.py
@@ -0,0 +1,23 @@
+
+# What is PIP?
+# PIP is a package manager for python packages or modules if you like.
+
+# Download a package - pip install camelcase
+
+
+# Using a Package - 
+
+# example: Import and use "camelcase"
+import camelcase
+
+c = camelcase.CamelCase()
+
+txt = "lorem ipsum dolor sit amet"
+
+print(c.hump(txt))
+
+#This method capitalizes the first letter of each word.
+
+
+
+# Remove a package - pip uninstall camelcase
