fc394ecca6e71316694a2acee66bd8984af7d7e0
w3schools-test <raeebikash@gmail.com>
2024-12-22 11:43:44 +0545

    Learning else & finally

--- DAY29_PythonTryExcept.py
+++ DAY29_PythonTryExcept.py
@@ -30,3 +30,42 @@ except:
     print("An error occurred")
 
 
+
+# Else - use else keyword to define a block of code to be executed if no errors were raised.
+
+try:
+    print("Hello, world!")
+except:
+    print("An exception occurred")
+else:
+    print("Nothing went wrong!") 
+
+
+
+
+# Finally - if specified , will be executed regardless if the try blcok raises an error or not.
+
+try:
+    print(x)
+except:
+    print("Something went wrong!")
+finally:
+    print("The 'try except' block is executed.")
+
+
+
+
+# Example: Try to open and write to a file that is not writable.
+
+try:
+    f = open("demofile.txt")
+    try:
+        f.write("Lorem Ipsum")
+    except:
+        print("Something went wrong when writing to the file.")
+
+    finally:
+        f.close()
+
+except: 
+    print("Something went wrong when opening the file.")
