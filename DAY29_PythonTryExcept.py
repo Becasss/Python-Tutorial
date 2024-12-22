1dc71c9f1373c0ec263d443105cb9960bd7d74d1
w3schools-test <raeebikash@gmail.com>
2024-12-22 11:36:19 +0545

    Learning exception Handling

--- DAY29_PythonTryExcept.py
+++ DAY29_PythonTryExcept.py
@@ -7,3 +7,26 @@
 
 # finally block lets you execute code, regardless of the result of the  try- and except blocks.
 
+
+
+
+# Exception Handling 
+
+# Example: Try block will generate an exception, because x isnot defined.
+
+try:
+    print(x)
+except: 
+    print("An exception occurred")
+
+
+# Print one message if the try block raises a NameError and another for other errors.
+
+try:
+    print(x)
+except NameError:
+    print("Variable x is not defined")
+except:
+    print("An error occurred")
+
+
