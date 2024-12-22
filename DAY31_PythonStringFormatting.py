6c63f944f585f20acbf2441deb99bb1f675c30a3
w3schools-test <raeebikash@gmail.com>
2024-12-22 12:57:31 +0545

    Learning multiple values , index numbers and named indexes

--- DAY31_PythonStringFormatting.py
+++ DAY31_PythonStringFormatting.py
@@ -85,3 +85,32 @@ print(txt.format(price))
 
 txt = "the price is {:.2f} dollars."
 print(txt.format(price))
+
+
+
+
+
+# Multiple Values - 
+
+quantity = 8
+itemno = 567
+price = 49.95
+
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity,itemno,price))
+
+
+
+
+# Index Numbers 
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity,itemno,price))
+
+
+
+# Named Indexes.
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "ford", model = "Mustang" ))
