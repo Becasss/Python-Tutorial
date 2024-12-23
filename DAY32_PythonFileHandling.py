e62be8da7cea604c64724204536954ef524c74ee
w3schools-test <raeebikash@gmail.com>
2024-12-23 11:13:36 +0545

    Learning syntax f = open(Filename)

--- DAY32_PythonFileHandling.py
+++ DAY32_PythonFileHandling.py
@@ -1,2 +1,28 @@
 
 # Python File Open - open() function takes two parameters ; filename and mode.
+
+
+
+
+# Syntax - f = open(filename).
+
+# code above is the same as : f = open(filename, "rt"). - "r" for read and "t" for text are the default values, you do not need to specify them.
+
+
+
+
+# Open a File on the server
+f = open("mymodule.py", "r")
+
+print(f.read())
+
+
+
+
+
+# Read only Parts of the file
+
+# Example: Return the 5 first characters of the file.
+f = open("mymodule.py", "r")
+
+print(f.read(10))
