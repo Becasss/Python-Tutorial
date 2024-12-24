7a659001cc9250be899754bd0640dd51a35ffd84
w3schools-test <raeebikash@gmail.com>
2024-12-24 12:38:05 +0545

    Creating day1pandasIntro.py

---
+++ PANDAS_Tutorial/DAY1_PandasItro.py
@@ -0,0 +1,6 @@
+
+'''Pandas is a Python library used for working with data sets.
+ Pandas is used to analyze data.
+ It has functions for analyzing, cleaning, exploring, and manipulating data.
+ 
+ Data Science : is a branch of computer science where we study how to store, use and analyze data for deriving information form it.'''
