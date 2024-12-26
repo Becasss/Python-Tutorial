190a0cf8262e55d8f9885b2c50553b39daf50f0c
w3schools-test <raeebikash@gmail.com>
2024-12-26 15:27:34 +0545

    Learning read json by pandas

--- PANDAS_Tutorial/DAY5_PandasReadJSON.py
+++ PANDAS_Tutorial/DAY5_PandasReadJSON.py
@@ -1,2 +1,7 @@
 
 # Read JSON - Big data sets often stored, or extracted as JSON.
+
+import pandas as pd
+df = pd.read_json('data.json')
+
+print(df.to_string())     # use to_string() to print the entire DataFrame.
