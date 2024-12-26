ccc7039d1e8627ce0c940009905cb9a87c91746d
w3schools-test <raeebikash@gmail.com>
2024-12-26 15:21:45 +0545

    Updating max_rows

--- PANDAS_Tutorial/DAY4_PandasReadCSV.py
+++ PANDAS_Tutorial/DAY4_PandasReadCSV.py
@@ -6,4 +6,23 @@
 # Example : Load the CSV into a DataFrame:
 import pandas as pd
 df = pd.read_csv('data.csv')
-print(df.to_string()) 
+print(df.to_string())   # Tip: Use to_string() to print the entire DataFrame.
+
+print(df)  # IF you have a large DataFrame with many rows, Pandas will only return the first 5 rows and the last 5 columns.
+
+
+
+# max_rows - pd.options.display.max_rows statement.
+
+import pandas as pd
+print(pd.options.display.max_rows)    # Output : 60
+
+
+
+
+# Example: Increase the maximum number of rows to display the entire DataFrame:
+
+import pandas as pd
+pd.options.display.max_rows = 9999
+df = pd.read_csv('data.csv')
+print(df)
