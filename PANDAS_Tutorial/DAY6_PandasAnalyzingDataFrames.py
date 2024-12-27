dd53018569617d954c9e36028d7921faab73966c
w3schools-test <raeebikash@gmail.com>
2024-12-27 14:13:46 +0545

    Create Day6PandasAnalyzingDataFrames.py

---
+++ PANDAS_Tutorial/DAY6_PandasAnalyzingDataFrames.py
@@ -0,0 +1,15 @@
+
+# Viewing The Data: One of the most used method for gettng a quick overview of the DataFrame, is the head() method.
+
+# head() method returns the headers and a specified number of rows, starting form the top.
+
+
+
+# Example: Get a quick overview by printing the first 10 rows of the DataFrame:
+
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+print(df.head(10))
+
