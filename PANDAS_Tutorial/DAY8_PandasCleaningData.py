46b2b4744db0ac4429a062112b01dd15449d67e6
w3schools-test <raeebikash@gmail.com>
2024-12-29 12:53:09 +0545

    Update

---
+++ PANDAS_Tutorial/DAY8_PandasCleaningData.py
@@ -0,0 +1,60 @@
+
+# Pandas - Cleaning Data
+
+# Data Cleaning - means fixing bad data in your data set and could be empty cells, data in wrong format, wrong data, duplicates.
+
+
+
+# Pandas  - Cleaning Empty Cells - empty cells can potentially give you a wrong result when you analyze data.
+
+
+
+# Remove Rows - one way to deal with empty cells is to remove rows that contain empty cells.
+
+
+# Example : Return a new Data Frame with no empty cells.
+
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+new_df = df.dropna()
+
+print(new_df.to_string()) 
+
+
+# Notice in the result that some rows have been removed (row 18, 22, and 28).
+# These rows had cells with empty values.
+
+# Note: By default, the dropna() method returns a new DataFrame and will not change the original.
+
+
+
+# Example: Remove all rows with NULL values:
+import pandas as pd
+df = pd.read_csv('data.csv')
+
+df.dropna(inplace = True) 
+print(df.to_string()) 
+
+# Notice in the result that some rows have been removed (row 18, 22, and 28).
+# These rows had cells with empty values.
+
+# Note: Now,the dropna(inplace = True) will NOT return a new DataFrame, but it will remove all rows containing NULL values form the original DataFrame.
+
+
+
+
+# Replace Empty Values - fillna() method allows us to replace emtpy values with a value:
+
+
+# Example: Replace NULL values with a number 130:
+
+import pandas as pd
+df = pd.read_csv('data.csv')
+
+df.fillna(130, inplace = True)
+
+print(df.to_string())
+
+# Notice in the result: empty cells got the value 130 (in row 18, 22, and 28).
