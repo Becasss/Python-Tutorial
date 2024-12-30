95ce09403f4cab525bb636714b94e9cee662567c
w3schools-test <raeebikash@gmail.com>
2024-12-30 13:17:28 +0545

    Learning how to remove rows

--- PANDAS_Tutorial/DAY7_PandasCleaningWrongFormat.py
+++ PANDAS_Tutorial/DAY7_PandasCleaningWrongFormat.py
@@ -39,4 +39,39 @@ print(df.to_string())
 
 
 
-#
+# Loop through all values in the "Duration" column.
+# If the value is higher than 120, set it to 120:
+
+
+import pandas as pd
+df = pd.read_csv('data.csv')
+
+for x in df.index:
+    if df.loc[x, "Duration"] > 120:
+        df.loc[x, "Duration"] = 120
+
+print(df.to_string())
+
+
+
+
+# Removing Rows - Another way of handling wrong data is to remove the rows that contains wrong data.
+
+# Example: Delete rows where "Duration" is higher than 120:
+
+import pandas as pd
+df = pd.read_csv('data.csv')
+
+for x in df.index:
+    if df.loc[x, "Duration"] > 120:
+        df.drop(x, inplace=True)
+
+# remember to include the inplace = Ture argument to make the changes in the original 
+# DataFrame object instead of returning a copy.
+
+
+print(df.to_string())
+
+
+
+
