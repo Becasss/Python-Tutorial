d76b77027609384be62ba08c0d0c7b8c7b6708f0
w3schools-test <raeebikash@gmail.com>
2024-12-31 17:29:33 +0545

    uPDATE FILE

--- PANDAS_Tutorial/DAY10_PandasPlotting.py
+++ PANDAS_Tutorial/DAY10_PandasPlotting.py
@@ -1,3 +1,25 @@
 
 # Plotting - Pandas uses the plot() method to create diagrams.
 
+
+
+# Example: Import pyplot form matplotlib and visualize our DataFrame.
+
+import sys
+import matplotlib
+matplotlib.use('Agg'
+               )
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data.csv')
+
+df.plot()
+plt.show()
+
+# Two lines to make our compiler able to draw:
+
+plt.savefig(sys.stdout.buffer)
+
+sys.stdout.flush()
+
