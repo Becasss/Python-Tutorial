fe3322cae0ac11a2df6eb35a7bf5b450a077fbcc
w3schools-test <raeebikash@gmail.com>
2025-01-02 13:23:24 +0545

    Create Day1_pytest.py

---
+++ Pytest/DAY1_Pytest.py
@@ -0,0 +1,11 @@
+
+'''Why Use Pytest?
+1. Ease of Use: Write test cases in simple Python functions without boilerplate code.
+2. Rich Features:
+    - Built-in fixtures for setup and teardown.
+    - Parameterized testing for multiple test scenarios.
+3. Extensibility: Supports plugins and custom functionality.
+4. Compatibility: Works well Selenium, Requests, and other QA tools.
+
+
+# Installing Pytest:'''
